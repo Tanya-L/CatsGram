11ac126b0cfefa8b8d58685bb4515d96b6afa501
Tanya Lytovchenko <tanya.lytovchenko@gmail.com>
2018-03-22 14:56:53 +0100

    More test

--- CatsGram/Test/testFormPost.py
+++ CatsGram/Test/testFormPost.py
@@ -1,22 +1,42 @@
-from selenium import webdriver
 import time
+import unittest
+
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
 
-driver.set_page_load_timeout(10)
-driver.get("http://127.0.0.1:8000/cattery/cat/1/")
+class SeleniumCatTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+        self.driver.set_page_load_timeout(10)
+
+    def tearDown(self):
+        self.driver.quit()
 
 
-def set_field(id, text):
-    driver.execute_script("document.getElementById('%s').value='%s'" % (id, text))
+class TestFormCatUpdate(SeleniumCatTestCase):
+
+    def __set_field__(self, id, text):
+        self.driver.execute_script("document.getElementById('%s').value='%s'" % (id, text))
+
+    def test_input_should_change_name_after_update(self):
+        #
+        # Given
+        #
+        name = "Bageera%s" % time.time()
+        self.driver.get("http://127.0.0.1:8000/cattery/cat/1/")
+
+        #
+        # When
+        #
+        # driver.find_element_by_id("cat_name_input").send_keys("Bageera")
+        self.__set_field__("cat_name_input", name)
+        self.driver.find_element_by_id("btnK").send_keys(Keys.ENTER)
 
 
-# driver.find_element_by_id("cat_name_input").send_keys("Bageera")
-name = "Bageera%s" % time.time()
-set_field("cat_name_input", name)
-driver.find_element_by_id("btnK").send_keys(Keys.ENTER)
-time.sleep(5)
-
-assert name == driver.find_element_by_id("cat_name").text
-driver.quit()
+        #
+        # Then
+        #
+        assert name == self.driver.find_element_by_id("cat_name").text
--- CatsGram/Test/testVotePolls.py
+++ CatsGram/Test/testVotePolls.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from selenium import webdriver
@@ -8,38 +9,38 @@ class SeleniumCatTestCase(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+
         self.driver.set_page_load_timeout(10)
 
     def tearDown(self):
-        self.driver.maximize_window()
         self.driver.refresh()
         self.driver.quit()
 
 
-class TestFindCattery(SeleniumCatTestCase):
+class TestVotePolls(SeleniumCatTestCase):
 
     def __set_field__(self, id, text):
-        self.driver.execute_script("document.getElementById('%s').value='%s'" % (id, text))
+        pass
 
-    def test_search_info_about_cattery(self):
+    def test_vote(self):
         #
         # Given
         #
-        self.driver.get("http://google.com")
+        self.driver.get("http://127.0.0.1:8000/polls/")
 
         #
         # When
         #
-        self.driver.find_element_by_name("q").send_keys("Cattery 'Naked Tails'")
-        self.driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+
+        self.driver.find_element_by_name("question1").click()
+        self.driver.find_element_by_id("choice3")
+        self.driver.find_element_by_css_selector("input[type='radio'][value='9']").click()
+
+        self.driver.find_element_by_name("VoteButton").send_keys(Keys.ENTER)
+
+        # self.driver.get("http://127.0.0.1:8000/polls/3/results/")
 
         #
         # Then
         #
         # assert name == self.driver.find_element_by_id("cat_name").text
-
-
-
-
-
-
